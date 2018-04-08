2f6191027c441e12ecf66132ea250fb0864e0658
Luan Devecchi <gitluan@engineer.com>
2018-04-08 09:28:47 -0300

    mini

---
+++ Internals.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueManager.Internals
+{
+    public enum Status
+    {
+        NL = 0,
+        L = 1
+    }
+    public class Team
+    {
+        public string name;
+        public int score;
+        public int reputation;
+        public int fans;
+    }
+
+    public class TeamPlayers
+    {
+        public string top;
+        public string mid;
+        public string jungler;
+        public string adcarry;
+        public string support;
+    }
+
+
+
+    public class Ext
+    {
+        public Status status = new Status();
+        public class Player
+        {
+            public string name;
+            public Team team;
+            public TeamPlayers team_players;
+        }
+
+    }
+}
