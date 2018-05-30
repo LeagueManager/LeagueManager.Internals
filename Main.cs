8c7d942adb42981983367ed04b9536bb2802f23a
Paulo Henrique <gladgamer01@gmail.com>
2018-05-30 15:12:10 -0300

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeagueManager.Internals
+{
+    public class Main
+    {
+        public Utils.MemoryProgram memoryProgram_Utils = new Utils.MemoryProgram();
+        public Internals.LoadJSON loadJson_Internal = new LoadJSON();
+    }
+}
