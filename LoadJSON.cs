f55e4e2154e177fedbb5f4fa87fddfdd7493f3bc
Paulo Henrique <gladgamer01@gmail.com>
2018-05-30 16:33:07 -0300

    Update LoadJSON.cs

--- LoadJSON.cs
+++ LoadJSON.cs
@@ -4,13 +4,14 @@ using Newtonsoft.Json;
 using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 
 namespace LeagueManager.Internals
 {
     public class LoadJSON
     {
         internal static string LoadFileInit = File.ReadAllText("leaguemanager.init");
-        internal static string[] Files_Init = LoadFileInit.Split(new string[] {"@\"" },StringSplitOptions.None);
+        internal static string[] Files_Init = LoadFileInit.Split(new string[] { "@\"" }, StringSplitOptions.None);
 
         internal string memoryString = String.Empty;
 
@@ -23,19 +24,36 @@ namespace LeagueManager.Internals
                 {
 
                     string a = Files_Init[i].Replace("\r\n", string.Empty);
-                    string b = a.Replace("\"\\",string.Empty);
+                    string b = a.Replace("\"\\", string.Empty);
                     string c = String.Empty;
-                    for(int x = 0; x < b.Length-1; x++)
+                    for (int x = 0; x < b.Length - 1; x++)
                     {
                         c = c + b[x];
                     }
 
                     memoryString = File.ReadAllText(c);
-                    main.memoryProgram_Utils.memoryStrings.Add(memoryString);
+                    main._memoryProgram.memoryStrings.Add(@memoryString);
                 }
-
             }
+            realiseConfig(main);
         }
-        
+
+        private void realiseConfig(Main main)
+        {
+            #region team
+            JArray jsonArray = JArray.Parse(@main._memoryProgram.memoryStrings[0]);
+            for (int i = 0; i < jsonArray.Count; i++)
+            {
+                var JSONFile = JObject.Parse(jsonArray[i].ToString());
+                main._teams.Add(JSONFile["name"].ToString(), new Utils.Team
+                {
+                    name = JSONFile["name"].ToString(),
+                    wins = int.Parse(JSONFile["wins"].ToString()),
+                    loses = int.Parse(JSONFile["loses"].ToString()),
+                    rank = int.Parse(JSONFile["rank"].ToString())
+                });
+            }
+            #endregion
+        }
     }
 }
