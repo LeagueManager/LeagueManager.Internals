310cd3e2fee435605ec88971b0d912ca159bcdbc
Paulo Henrique <gladgamer01@gmail.com>
2018-06-03 16:53:28 -0300

    Update LoadJSON.cs

--- LoadJSON.cs
+++ LoadJSON.cs
@@ -11,48 +11,51 @@ namespace LeagueManager.Internals
     public class LoadJSON
     {
         internal static string LoadFileInit = File.ReadAllText("leaguemanager.init");
-        internal static string[] Files_Init = LoadFileInit.Split(new string[] { "@\"" }, StringSplitOptions.None);
+        internal static string[] Files_Init = LoadFileInit.Split(new string[] { "0x1" }, StringSplitOptions.None);
 
         internal string memoryString = String.Empty;
 
         public void mainActivity(Main main)
         {
-            for (int i = 0; i < Files_Init.Length; i++)
+            for (int i = 0; i < Files_Init.Length-1; i++)
             {
-
-                if (i >= 1)
-                {
-
-                    string a = Files_Init[i].Replace("\r\n", string.Empty);
-                    string b = a.Replace("\"\\", string.Empty);
-                    string c = String.Empty;
-                    for (int x = 0; x < b.Length - 1; x++)
-                    {
-                        c = c + b[x];
-                    }
-
-                    memoryString = File.ReadAllText(c);
-                    main._memoryProgram.memoryStrings.Add(@memoryString);
-                }
+                if(i>0)
+                memoryString = File.ReadAllText(String.Concat(Files_Init[0],"\\",Files_Init[i]));
+                main._memoryProgram.memoryStrings.Add(@memoryString);
             }
             realiseConfig(main);
         }
 
         private void realiseConfig(Main main)
         {
+            main._memoryProgram.memoryStrings.Remove("");
             #region team
-            JArray jsonArray = JArray.Parse(@main._memoryProgram.memoryStrings[0]);
-            for (int i = 0; i < jsonArray.Count; i++)
+            JArray jsonArrayTeam = JArray.Parse(@main._memoryProgram.memoryStrings[0]);
+            for (int i = 0; i < jsonArrayTeam.Count; i++)
             {
-                var JSONFile = JObject.Parse(jsonArray[i].ToString());
-                main._teams.Add(JSONFile["name"].ToString(), new Utils.Team
+                var JSONFileTeam = JObject.Parse(jsonArrayTeam[i].ToString());
+                main._teams.Add(JSONFileTeam["name"].ToString(), new Utils.Team
                 {
-                    name = JSONFile["name"].ToString(),
-                    wins = int.Parse(JSONFile["wins"].ToString()),
-                    loses = int.Parse(JSONFile["loses"].ToString()),
-                    rank = int.Parse(JSONFile["rank"].ToString())
+                    name = JSONFileTeam["name"].ToString(),
+                    wins = int.Parse(JSONFileTeam["wins"].ToString()),
+                    loses = int.Parse(JSONFileTeam["loses"].ToString()),
+                    rank = int.Parse(JSONFileTeam["rank"].ToString())
                 });
             }
+            #endregion
+
+            #region user
+            JArray jsonArrayUserData = JArray.Parse(@main._memoryProgram.memoryStrings[1]);
+                var JSONFileUser = JObject.Parse(jsonArrayUserData[0].ToString());
+                main._user.Name = JSONFileUser["name"].ToString();
+                JSONFileUser = JObject.Parse(jsonArrayUserData[1].ToString());
+            main._user.Team = new Utils.Team()
+            {
+                name = JSONFileUser["name"].ToString(),
+                wins = int.Parse(JSONFileUser["wins"].ToString()),
+                loses = int.Parse(JSONFileUser["loses"].ToString()),
+                rank = int.Parse(JSONFileUser["rank"].ToString())
+            };
             #endregion
         }
     }
